acaa828406e0b92c54d125bbced17a327b7e5e2e
Gabriel Cesar Mello <95gabrielcesar@gmail.com>
2021-08-18 18:25:47 -0300

    feat(Structure): 🎨 proper styled components coding alongside twin

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from './routes';
-import AppStyles from "./AppStyles.styles.tw";
+import AppStyles from './AppStyles.styles.tw';
 
 function App() {
   return (
--- src/AppStyles.styles.tw.ts
+++ src/AppStyles.styles.tw.ts
@@ -1,30 +1,34 @@
 import styled from "styled-components";
-import "./index.css"
 import tw from "twin.macro";
+// talvez nem precise:
+import "./index.css";
 
-const AppStyles = styled.div.attrs({
-    className: "w-full h-screen flex flex-col items-center justify-center bg-black"
-  })`
+
+export const AppStyles = styled.a`
+    ${tw`w-full h-screen flex flex-col items-center justify-center bg-black`}
+    
     & {
-      h1 {
-          ${tw`text-6xl text-6xl text-red-500`}
-          text-shadow: 10px 10px 2px rgba(150, 150, 150, 1);
-      }
-      p {
-        ${tw`text-gray-700 text-lg`}
-      }
-      h2 {
-        ${tw`text-2xl mt-5 text-red-500`}
-      }
-      ul {
-        ${tw`inline-flex`}
-      }
-      li {
-        ${tw`mr-5`}
-      }
-      a {
-        ${tw`text-blue-500 hover:text-gray-500 hover:underline`}
-      }
+        h1 {
+            ${tw`text-6xl text-6xl text-red-500`}
+            text-shadow: 4px 4px 2px rgba(150, 150, 150, 1);
+        }
+        p {
+            ${tw`text-gray-700 text-lg`}
+        }
+        h2 {
+            ${tw`text-2xl mt-5 text-red-500`}
+        }
+        ul {
+            ${tw`inline-flex`}
+        }
+        li {
+            ${tw`mr-5`}
+        }
+        a {
+            ${tw`text-blue-500 hover:text-gray-500 hover:underline`}
+        }
     }
-  `;
-  export default AppStyles;
+
+`;
+
+export default AppStyles;
