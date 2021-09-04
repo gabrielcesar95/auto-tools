58d0b8f72196b231ee413a31560b2e98d964437a
Gabriel Cesar Mello <95gabrielcesar@gmail.com>
2021-09-04 02:03:52 -0300

    refactor(FuelComparer): ♻️ trying to make the form work

--- src/pages/Tools/FuelComparer/components/FuelInputBlock.tsx
+++ src/pages/Tools/FuelComparer/components/FuelInputBlock.tsx
@@ -3,18 +3,27 @@ import { FuelArticle, InputDiv } from '../styles';
 import PropTypes, { InferProps } from 'prop-types';
 import FuelType from '../../../../models/FuelType';
 
+export class ConsumptionData {
+    price = 0;
+    consumption = 0;
+
+    constructor(price: number, consumption: number) {
+        this.price = price;
+        this.consumption = consumption;
+    }
+}
+
 const propTypes = {
-    fuelType: PropTypes.instanceOf(FuelType).isRequired
+    fuelType: PropTypes.instanceOf(FuelType).isRequired,
+    data: PropTypes.instanceOf(ConsumptionData).isRequired,
+    setData: PropTypes.func.isRequired
 };
 
 type FuelInputBlockPropTypes = InferProps<typeof propTypes>;
 
-const FuelInputBlock: React.FC<FuelInputBlockPropTypes> = ({ fuelType }: FuelInputBlockPropTypes) => {
+const FuelInputBlock: React.FC<FuelInputBlockPropTypes> = ({ fuelType, data, setData }: FuelInputBlockPropTypes) => {
     const input_price_id = `fuel_price[${fuelType.id}]`;
     const input_consumption_id = `fuel_consumption[${fuelType.id}]`;
-
-    const [price, setPrice] = useState<number>();
-    const [consumption, setConsumption] = useState<number>();
 
     return (
         <FuelArticle>
@@ -28,8 +37,8 @@ const FuelInputBlock: React.FC<FuelInputBlockPropTypes> = ({ fuelType }: FuelInp
                     id={input_price_id}
                     step=".01"
                     min="0"
-                    value={price}
-                    onChange={(e) => setPrice(Number(e.target.value))}
+                    value={data?.price}
+                    onChange={(e) => setData(new ConsumptionData(Number(e.target.value), data.consumption))}
                 />
             </InputDiv>
             <InputDiv>
@@ -41,8 +50,8 @@ const FuelInputBlock: React.FC<FuelInputBlockPropTypes> = ({ fuelType }: FuelInp
                     id={input_consumption_id}
                     step=".1"
                     min="0"
-                    value={consumption}
-                    onChange={(e) => setConsumption(Number(e.target.value))}
+                    value={data?.consumption}
+                    onChange={(e) => setData(new ConsumptionData(data.price, Number(e.target.value)))}
                 />
             </InputDiv>
         </FuelArticle>
--- src/pages/Tools/FuelComparer/index.tsx
+++ src/pages/Tools/FuelComparer/index.tsx
@@ -2,28 +2,28 @@ import React, { useState } from 'react';
 import AppHeader from '../../../components/Header';
 import AppStyles from '../../../components/styles';
 import FuelType from '../../../models/FuelType';
-import FuelInputBlock from './components/FuelInputBlock';
+import FuelInputBlock, { ConsumptionData } from './components/FuelInputBlock';
 import Results from './components/Results';
 import { ButtonsDiv, InputSection } from './styles';
+
+interface ExpenseEstimate {
+    fuelType: FuelType;
+    pricePerKm: number;
+}
+interface Results {
+    recommendedFuelType: FuelType;
+    expenseEstimates: ExpenseEstimate[];
+}
 
 const FuelComparer: React.FC = () => {
     // TODO: pegar fueltypes do firebase
     const fuelTypes = [new FuelType('gasoline', 'Gasolina'), new FuelType('ethanol', 'Etanol')];
 
-    interface ExpenseEstimate {
-        fuelType: FuelType;
-        pricePerKm: number;
-    }
-    interface Results {
-        recommendedFuelType: FuelType;
-        expenseEstimates: ExpenseEstimate[];
-    }
+    const [comparisonData, setComparisonData] = useState({});
 
     const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
         e.preventDefault();
     };
-
-    // const compare = () => {};
 
     return (
         <AppStyles>
@@ -32,9 +32,20 @@ const FuelComparer: React.FC = () => {
             <main>
                 <form onSubmit={handleSubmit}>
                     <InputSection>
-                        {fuelTypes.map((fuelType) => (
-                            <FuelInputBlock fuelType={fuelType} key={fuelType.id} />
-                        ))}
+                        {fuelTypes.map((fuelType) => {
+                            const [formBlockData, setFormBlockData] = useState<ConsumptionData>(
+                                new ConsumptionData(0, 0)
+                            );
+
+                            return (
+                                <FuelInputBlock
+                                    fuelType={fuelType}
+                                    key={fuelType.id}
+                                    data={formBlockData}
+                                    setData={setFormBlockData}
+                                />
+                            );
+                        })}
                     </InputSection>
                     <ButtonsDiv>
                         <button type="submit">Comparar</button>
